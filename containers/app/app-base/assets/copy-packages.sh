2a07f45abfb526b77fe3f96ae608fccc7f190115
Marcelo Linhares Castro <marlinhares@gmail.com>
2025-06-11 14:29:16 -0300

    Ajuste no app-dev do SEI4 para aceitar conexões Oracle PDO (#135)

--- containers/app/app-base/assets/copy-packages.sh
+++ containers/app/app-base/assets/copy-packages.sh
@@ -14,6 +14,8 @@ curl -L -O \
     -O \
     https://github.com/spbgovbr/sei-docker-binarios/raw/main/pacoteslinux/oracle-instantclient12.2-sqlplus-12.2.0.1.0-1.x86_64.rpm \
     -O \
+    https://github.com/spbgovbr/sei-docker-binarios/raw/main/pacoteslinux/php-7.3.33.tar.gz \
+    -O \
     https://github.com/spbgovbr/sei-docker-binarios/raw/main/pacoteslinux/uploadprogress.tgz \
     -O \
     https://github.com/spbgovbr/sei-docker-binarios/raw/main/pacoteslinux/wkhtmltox-0.12.6-1.centos7.x86_64.rpm 
