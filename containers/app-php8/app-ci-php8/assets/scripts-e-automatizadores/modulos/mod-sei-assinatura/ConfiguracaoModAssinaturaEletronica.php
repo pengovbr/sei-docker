8c7fa93c9d6a861fc1a47eb1b0c9c68d05359e39
Marcelo Linhares Castro <marlinhares@gmail.com>
2026-06-02 20:57:39 -0300

    Feat/marlinhares 169 mod assinatura (#170)
    * Nova Estrutura do Modulo de Assinatura

--- containers/app-php8/app-ci-php8/assets/scripts-e-automatizadores/modulos/mod-sei-assinatura/ConfiguracaoModAssinaturaEletronica.php
+++ containers/app-php8/app-ci-php8/assets/scripts-e-automatizadores/modulos/mod-sei-assinatura/ConfiguracaoModAssinaturaEletronica.php
@@ -3,7 +3,7 @@
 /**
  * Arquivo de configuração do Módulo de Integração do SEI com a plataforma de Assinatura Avançada do gov.br
  *
- * Seu desenvolvimento seguiu os mesmos padrÃµes de configuração implementado pelo SEI e SIP e este
+ * Seu desenvolvimento seguiu os mesmos padrões de configuração implementado pelo SEI e SIP e este
  * arquivo precisa ser adicionado à pasta de configurações do SEI para seu correto carregamento pelo módulo.
  */
 
@@ -33,28 +33,61 @@ class ConfiguracaoModAssinaturaEletronica extends InfraConfiguracao
     {
       return array(
           'AssinaturaAvancada' => array(
+              // Endereço para acesso à API do serviço de assinatura avançada.
+              //Os endereços disponíveis são os seguintes (verifique se houve atualizações durante o procedimento de instalação):
+              //     - Homologação: https://cas.staging.iti.br/oauth2.0
+              //     - Produção: https://cas.iti.br/oauth2.0
               'url_provider' => getenv('MODULO_ASSINATURA_URLPROVIDER'),
+
+              // Chave de acesso, que identifica a aplicação consumidora devidamente autorizada para acessar os serviços de assinatura eletrônica.
               'client_id' => getenv('MODULO_ASSINATURA_CLIENTID'),
-              'secret' => getenv('MODULO_ASSINATURA_SECRET'),
+
+              // Senha de acesso da aplicação consumidora.
+              'secret'    => getenv('MODULO_ASSINATURA_SECRET'),
+
           ),
           'ValidarAPI' => array(
+             // Endereço para acesso à API do serviço Validar.
+             // Os endereços disponíveis são os seguintes (verifique se houve atualizações durante o procedimento de instalação):
+             //     - Homologação: https://h-api.iti.gov.br/validar/v3/validar
+            //      - Produção: https://api.iti.gov.br/validar/v3/validar
             'url' => getenv('MODULO_ASSINATURA_VALIDAR_API_URL'),
+            // Chave de acesso da API.
             'key' => getenv('MODULO_ASSINATURA_VALIDAR_API_KEY'),
           ),
           'Assinador' => array(
-            'Token' => array(
-                'url' => getenv('MODULO_ASSINATURA_TOKEN_URL'),
-                'sign_url' => getenv('MODULO_ASSINATURA_TOKEN_SIGN_URL'),
+            'config' => array(
+                // OID da suíte criptográfica a ser utilizada para assinatura.
+                'suite' => getenv('MODULO_ASSINATURA_SUITE'), // algoritmo sha256WithRSA.
+            ),
+            'Pkcs12' => array(
+                // Endereço para acesso à API do serviço IntegraICP.
+                'url' => getenv('MODULO_ASSINATURA_PKCS12_URL'),
+                // URL pra enviar arquivo a ser assinado.
+                'sign_url' => getenv('MODULO_ASSINATURA_PKCS12_URL_ASSINAR'),
+            ),
+            'Ykue' => array(
+                // Endereço para acesso à API do serviço Ykue.
+                'url' => getenv('MODULO_ASSINATURA_YKUE_URL'),
+                // URL pra enviar arquivo a ser assinado.
+                'sign_url' => getenv('MODULO_ASSINATURA_YKUE_URL_ASSINAR'),
             ),
             'IntegraICP' => array(
+                // Endereço para acesso à API do serviço IntegraICP.
                 'url' => getenv('MODULO_ASSINATURA_INTEGRA_ICP_URL'),
+                // URL pra retornar lista de autenticações do CPF.
                 'clearings_url' => getenv('MODULO_ASSINATURA_INTEGRA_ICP_URL_CLEARINGS'),
+                // URL pra enviar arquivo a ser assinado.
                 'sign_url' => getenv('MODULO_ASSINATURA_INTEGRA_ICP_URL_ASSINAR'),
             ),
             'CloudPSC' => array(
+              // Endereço para acesso à API do serviço Cloud PSC.
               'url' => getenv('MODULO_ASSINATURA_CLOUDPSC_URL'),
-              'start_url' => getenv('MODULO_ASSINATURA_CLOUDPSC_START_URL'),
-              'sign_url' => getenv('MODULO_ASSINATURA_CLOUDPSC_SIGN_URL'),
+              // URL pra retornar lista de autenticações.
+              'start_url' => getenv('MODULO_ASSINATURA_CLOUDPSC_URL_START'),
+              // URL pra enviar arquivo a ser assinado.
+              'sign_url' => getenv('MODULO_ASSINATURA_CLOUDPSC_URL_ASSINAR'),
+              // Opções de assinadores do Cloud PSC ['safeweb', 'soluti', 'govbr', 'serpro'],
               'options' =>  ['govbr', 'serpro'],
             ),
             'apikey' => getenv('MODULO_ASSINATURA_API_KEY_ITYHY'),
